1b89d80fa4df784c5fdb82ecc19eee68d6521e14
ayushx4 <ayushvadodariya12@gamil.com>
2024-12-19 23:42:03 +0530

    Set up React with TypeScript and Tailwind CSS

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center bg-black-primary">
+      <div className="w-3/4 h-3/4 bg-slate-800">
+        <div className=" w-1/3"></div>
+        <div className="flex-1 bg-slate-800">
+      </div>
+
+      </div>
+    </div>
+  );
+}
+
+export default App
